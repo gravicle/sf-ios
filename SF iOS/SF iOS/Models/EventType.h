b6959791944157a5c4478686d9a0231a9de29090
Amit Jain <gravicle@gmail.com>
2017-07-30 02:27:57 -0700

    Add Event and location models

---
+++ SF iOS/SF iOS/Models/EventType.h
@@ -0,0 +1,11 @@
+//
+//  EventType.h
+//  SF iOS
+//
+//  Created by Amit Jain on 7/29/17.
+//  Copyright © 2017 Amit Jain. All rights reserved.
+//
+
+typedef NS_ENUM(NSUInteger, EventType) {
+    EventTypeSFCoffee = 0
+};
