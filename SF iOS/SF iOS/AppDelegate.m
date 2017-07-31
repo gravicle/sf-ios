426052acdfbddec2f4e83c4620ac6441f6c3ebc1
Amit Jain <gravicle@gmail.com>
2017-07-31 10:33:39 -0700

    Remove template generated Storyboard and Viewcontroller

--- SF iOS/SF iOS/AppDelegate.m
+++ SF iOS/SF iOS/AppDelegate.m
@@ -20,6 +20,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UIViewController alloc] init];
+    self.window.rootViewController.view.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
     return true;
 }
 
