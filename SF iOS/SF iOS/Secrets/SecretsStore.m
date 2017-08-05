36751d39bc26836312d82887c8671ac9a879cae8
Amit Jain <gravicle@gmail.com>
2017-08-05 12:18:19 -0700

    Add secrets access wrapper

---
+++ SF iOS/SF iOS/Secrets/SecretsStore.m
@@ -0,0 +1,52 @@
+//
+//  SecretsStore.m
+//  SF iOS
+//
+//  Created by Amit Jain on 8/5/17.
+//  Copyright © 2017 Amit Jain. All rights reserved.
+//
+
+#import "SecretsStore.h"
+
+@interface SecretsStore ()
+
+@property (nonatomic) NSDictionary<NSString *, NSString *> *secrets;
+
+@end
+
+@implementation SecretsStore
+
+- (instancetype)init {
+    if (self = [super init]) {
+        NSString *plistPath = [[NSBundle mainBundle] pathForResource:secretsFileName ofType:@"plist"];
+        self.secrets = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+        NSAssert(self.secrets != nil, @"Configure secrets by following instructions in Readme.");
+    }
+    return self;
+}
+
+- (NSString *)uberClientID {
+    return self.secrets[@"uber-client-id"];
+}
+
+- (NSString *)uberClientSecret {
+    return self.secrets[@"uber-client-secret"];
+}
+
+- (NSString *)uberServerToken {
+    return self.secrets[@"uber-server-token"];
+}
+
+- (NSString *)lyftClientID {
+    return self.secrets[@"lyft-client-id"];
+}
+
+- (NSString *)lyftClientSecret {
+    return self.secrets[@"lyft-client-secret"];
+}
+
+-(NSString *)lyftServerToken {
+    return self.secrets[@"lyft-sever-token"];
+}
+
+@end
