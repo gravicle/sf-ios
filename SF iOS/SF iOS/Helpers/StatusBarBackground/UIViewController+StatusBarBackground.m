2b55e236cf44b4c5bb17d649b65171ce753e269b
Nealon Young <hi@nealyoung.me>
2019-03-29 16:30:10 -0700

    Use safeAreaLayoutGuide top anchor to position bottom of status bar overlay view

--- SF iOS/SF iOS/Helpers/StatusBarBackground/UIViewController+StatusBarBackground.m
+++ SF iOS/SF iOS/Helpers/StatusBarBackground/UIViewController+StatusBarBackground.m
@@ -15,12 +15,12 @@
     UIVisualEffectView *statusBarBackground = [[UIVisualEffectView alloc] initWithEffect:effect];
     statusBarBackground.translatesAutoresizingMaskIntoConstraints = false;
     [self.view addSubview:statusBarBackground];
+
     [statusBarBackground.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = true;
+    [statusBarBackground.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor].active = true;
     [statusBarBackground.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = true;
     [statusBarBackground.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = true;
-    [statusBarBackground.heightAnchor constraintEqualToConstant:[self statusBarHeight]].active = true;
 }
-
 
 - (CGFloat)statusBarHeight {
     return [UIApplication sharedApplication].statusBarFrame.size.height;
