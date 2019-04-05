98aa2b2c6153fa2b4bebddec9d3e8bbf00309987
Roderic Campbell <roderic@gmail.com>
2019-04-05 16:25:57 -0700

    Update tests to be less context sensitive
    fixes #4

--- SF iOS/SF iOSTests/DateTests.m
+++ SF iOS/SF iOSTests/DateTests.m
@@ -10,32 +10,33 @@
 #import "NSDate+Utilities.h"
 
 @interface DateTests : XCTestCase
-
 @end
 
 @implementation DateTests
 
 - (void)testLaterThanDate {
-    NSDate *now = [NSDate new];
+    NSDate *now = [[NSDate alloc] initWithTimeIntervalSince1970:2000];
     NSDate *later = [now dateByAddingTimeInterval:1000];
     XCTAssertTrue([later isLaterThanDate:now]);
 }
 
 - (void)testBefioreThanDate {
-    NSDate *now = [NSDate new];
+    NSDate *now = [[NSDate alloc] initWithTimeIntervalSince1970:2000];
     NSDate *earlier = [now dateByAddingTimeInterval:-01000];
     XCTAssertTrue([earlier isEarlierThanDate:now]);
 }
 
 - (void)testTimeSlotWhenStartAndEndDatesAreInTheSamePeriod {
-    NSDate *start = [[NSCalendar currentCalendar] startOfDayForDate:[NSDate new]];
+    NSDate *now = [[NSDate alloc] initWithTimeIntervalSince1970:2000];
+    NSDate *start = [[NSCalendar currentCalendar] startOfDayForDate:now];
     NSString *timesot = [NSDate timeslotStringFromStartDate:start duration:1800];
     
     XCTAssertTrue([@"12:00 - 12:30AM" isEqualToString:timesot]);
 }
 
 - (void)testTimeSlotWhenStartAndEndDatesAreInDifferentPeriod {
-    NSDate *start = [[NSCalendar currentCalendar] startOfDayForDate:[NSDate new]];
+    NSDate *now = [[NSDate alloc] initWithTimeIntervalSince1970:2000];
+    NSDate *start = [[NSCalendar currentCalendar] startOfDayForDate:now];
     NSString *timesot = [NSDate timeslotStringFromStartDate:start duration:13 * 60 * 60]; // 13hrs later
     
     XCTAssertTrue([@"12:00AM - 1:00PM" isEqualToString:timesot]);
