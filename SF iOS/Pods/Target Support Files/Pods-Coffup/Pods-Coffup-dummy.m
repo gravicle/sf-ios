1142e765facb001433156451dd7abae629974d12
Roderic Campbell <roderic@gmail.com>
2019-04-11 18:27:25 -0700

    Add cocoapods to gemfile. Update cocoapods

---
+++ SF iOS/Pods/Target Support Files/Pods-Coffup/Pods-Coffup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Coffup : NSObject
+@end
+@implementation PodsDummy_Pods_Coffup
+@end
--- SF iOS/Pods/Target Support Files/Pods-SF iOS/Pods-SF iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SF_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_SF_iOS
-@end
